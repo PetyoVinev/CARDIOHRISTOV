f047ea094bc22057d3819e15173912a256d0dc47
Petyo Vinev <petyo.vinev@gmail.com>
2019-04-05 10:42:36 +0300

    pass CSRFToken with Ajax post request for edit roles

--- src/main/resources/static/js/change-user-role.js
+++ src/main/resources/static/js/change-user-role.js
@@ -3,16 +3,22 @@ $(function () {
     $('#role').change(function () {
         var userEmail = document.getElementById("emailRegister").value;
         var role =  $("#role option:selected").text();
+        let _csrf  = $('input[name=_csrf]').val();
 
         var data = {
             email: userEmail,
-            role: role
+            role: role,
+            _csrf:_csrf
         };
+
 
         $.ajax({
             type : 'POST',
             contentType: 'application/json',
             url: 'http://localhost:8000/profile/roleEdit',
+            headers: {
+                'X-CSRF-Token': _csrf
+            },
             data: data,
             success: function (success) {
                 console.log("success");
