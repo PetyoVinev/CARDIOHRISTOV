f9aea59eb67ef49d573bffbde87a5eabeae9e87a
Petyo Vinev <petyo.vinev@gmail.com>
2019-04-12 08:17:06 +0300

    edit Procedure entity

--- src/main/java/org/vinevweb/cardiohristov/domain/entities/Procedure.java
+++ src/main/java/org/vinevweb/cardiohristov/domain/entities/Procedure.java
@@ -13,19 +13,20 @@ import java.time.LocalDateTime;
 @Table(name = "procedures")
 public class Procedure extends BaseEntity {
 
-
+    @Column(name = "name", nullable = false)
     private String name;
 
     private String content;
 
+    @Column(name = "date", nullable = false)
     private LocalDateTime date;
 
+    @Column(name = "image_url", nullable = true)
     private String pictureUrl;
 
     public Procedure() {
     }
 
-    @Column(name = "name", nullable = false)
     public String getName() {
         return name;
     }
@@ -43,7 +44,6 @@ public class Procedure extends BaseEntity {
         this.content = content;
     }
 
-    @Column(name = "date", nullable = false)
     public LocalDateTime getDate() {
         return date;
     }
@@ -52,7 +52,6 @@ public class Procedure extends BaseEntity {
         this.date = date;
     }
 
-    @Column(name = "image_url", nullable = true)
     public String getPictureUrl() {
         return pictureUrl;
     }
